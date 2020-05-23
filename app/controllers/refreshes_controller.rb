88231c0ca81f8999abfa4bd999b29a5e9d9c30ae
Danilo de Lima <danilolicd@gmail.com>
2020-05-23 17:04:03 -0300

    refactor controllers

--- app/controllers/refreshes_controller.rb
+++ app/controllers/refreshes_controller.rb
@@ -4,7 +4,13 @@ class RefreshesController < ApplicationController
   before_action :authorize_refresh_request!
 
   def create
-    session = JWTSessions::Session.new(payload: payload)
+    session = JWTSessions::Session.new(payload: create_payload)
     render json: session.refresh(found_token)
+  end
+
+  private
+
+  def create_payload
+    payload.merge({ user_id: params[:user_id] })
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,16 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by!(email: params[:email])
     if @user.authenticate(params[:password])
-      payload = { user_id: @user.id }
-      session = JWTSessions::Session.new(payload: payload)
-      render json: session.login.merge({ role: 'admin' })
+      @session = JWTSessions::Session.new(payload: create_payload(@user))
+      render json: @session.login
     else
       render json: 'Invalid user', status: :unauthorized
     end
+  end
+
+  private
+
+  def create_payload(user)
+    { user_id: user.id, role: 'admin' }
   end
 end
